a2f5e3b8a547fb1227d8c7869e2b4c91bbad0567
amirberernji <ahberenji@gmail.com>
2025-08-27 08:53:04 +0330

    let's change a bit

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"ok": True, "msg": "Hello from FastAPI template and it`s working and have ci/cd!"}
+    return {"ok": True, "msg": "FastAPI CI/CD template is working!"}
